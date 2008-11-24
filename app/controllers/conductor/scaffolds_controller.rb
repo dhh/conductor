8203ce36d93361c6b52d4cccc994f2bb93abd840
David Heinemeier Hansson <david@loudthinking.com>
2008-11-24 13:08:47 +0100

    Added quick scaffold hack

--- app/controllers/conductor/scaffolds_controller.rb
+++ app/controllers/conductor/scaffolds_controller.rb
@@ -1,7 +1,21 @@
+require 'rails_generator'
+require 'rails_generator/scripts/generate'
+
 class Conductor::ScaffoldsController < Conductor::BaseController
   def new
   end
   
-  def create    
+  def create
+    Rails::Generator::Scripts::Generate.new.run(generator_arguments)
+    redirect_to(new_conductor_scaffold_url)
   end
+  
+  
+  private
+    def generator_arguments
+      returning [] do |args|
+        args << params[:scaffold][:name]
+        args << params[:scaffold][:fields].select { |f| f[:name].blank? }.collect { |f| "#{f[:name]}:#{f[:type]}" }
+      end.flatten
+    end
 end
