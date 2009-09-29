5574f8ddd1f74d6f74bd0cb0cabe56bf27cc44be
Joshua Peek <josh@joshpeek.com>
2009-09-28 23:07:57 -0500

    Disable sessions

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
+  config.action_controller.session_store = nil
   config.time_zone = 'UTC'
 end
