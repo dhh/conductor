d9a9669efa3390ba28904f5587eeb316286e1a48
David Heinemeier Hansson <david@loudthinking.com>
2010-04-11 16:45:42 -0700

    Rails 3 style routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
-  map.namespace :conductor do |conductor|
-    conductor.resources :scaffolds, :routes, :annotations
+Rails.application.routes.draw do
+  namespace :conductor do
+    resources :scaffolds, :routes, :annotations
   end
 end
