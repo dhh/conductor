7e5a49eeb2b61e40ad967fb6bdf48cf2a4b49f01
David Heinemeier Hansson <david@loudthinking.com>
2010-04-11 16:45:53 -0700

    Clean out old config

--- config/environment.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), 'boot')
-
-Rails::Initializer.run do |config|
-  config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-  config.action_controller.session_store = nil
-  config.time_zone = 'UTC'
-end
