a65ff647c118a3752a6fdabe247b1a3f9d2a1229
David Heinemeier Hansson <david@loudthinking.com>
2010-04-11 17:15:25 -0700

    Fixed scaffold generator

--- app/controllers/conductor/scaffolds_controller.rb
+++ app/controllers/conductor/scaffolds_controller.rb
@@ -1,5 +1,4 @@
-require 'rails_generator'
-require 'rails_generator/scripts/generate'
+require 'rails/generators'
 
 module Conductor
   class ScaffoldsController < ApplicationController
@@ -8,19 +7,19 @@ module Conductor
   
     def create
       # TODO: Change to use the proper ScaffoldGenerator instead of this CLI hack job
-      Rails::Generator::Scripts::Generate.new.run(generator_arguments)
+      Rails::Generators.invoke("scaffold", generator_arguments, :behavior => :invoke, :destination_root => Rails.root)
       redirect_to(new_conductor_scaffold_url)
     end
   
   
     private
       def generator_arguments
-        returning [ "scaffold" ] do |args|
+        returning(Array.new) do |args|
           args << params[:scaffold][:name]
           args << "--skip-timestamps" if params[:scaffold][:skip_timestamps] == "1"
           args << "--skip-migration" if params[:scaffold][:skip_migration] == "1"
           args << params[:scaffold][:fields].select { |f| f[:name].blank? }.collect { |f| "#{f[:name]}:#{f[:type]}" }
-        end.flatten
+        end
       end
   end
 end
