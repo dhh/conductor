9615923466adb675225a76a28dc6829dc189400e
David Heinemeier Hansson <david@loudthinking.com>
2010-04-11 17:05:36 -0700

    Use ApplicationController instead of BaseController

--- app/controllers/conductor/annotations_controller.rb
+++ app/controllers/conductor/annotations_controller.rb
@@ -1,9 +1,11 @@
 require 'rails/source_annotation_extractor'
 
-class Conductor::AnnotationsController < Conductor::BaseController
-  # TODO: Make it include all the default annotation forms
-  def index
-    # TODO: Move SourceAnnotationExtractor from Railties to ActiveSupport::SourceAnnotationExtractor
-    @notes = SourceAnnotationExtractor.new("TODO").find(File.join(Rails.root, 'app'))
+module Conductor
+  class AnnotationsController < ApplicationController
+    # TODO: Make it include all the default annotation forms
+    def index
+      # TODO: Move SourceAnnotationExtractor from Railties to ActiveSupport::SourceAnnotationExtractor
+      @notes = SourceAnnotationExtractor.new("TODO").find(File.join(Rails.root, 'app'))
+    end
   end
 end
--- app/controllers/conductor/scaffolds_controller.rb
+++ app/controllers/conductor/scaffolds_controller.rb
@@ -1,24 +1,26 @@
 require 'rails_generator'
 require 'rails_generator/scripts/generate'
 
-class Conductor::ScaffoldsController < Conductor::BaseController
-  def new
-  end
-  
-  def create
-    # TODO: Change to use the proper ScaffoldGenerator instead of this CLI hack job
-    Rails::Generator::Scripts::Generate.new.run(generator_arguments)
-    redirect_to(new_conductor_scaffold_url)
-  end
-  
-  
-  private
-    def generator_arguments
-      returning [ "scaffold" ] do |args|
-        args << params[:scaffold][:name]
-        args << "--skip-timestamps" if params[:scaffold][:skip_timestamps] == "1"
-        args << "--skip-migration" if params[:scaffold][:skip_migration] == "1"
-        args << params[:scaffold][:fields].select { |f| f[:name].blank? }.collect { |f| "#{f[:name]}:#{f[:type]}" }
-      end.flatten
+module Conductor
+  class ScaffoldsController < ApplicationController
+    def new
     end
+  
+    def create
+      # TODO: Change to use the proper ScaffoldGenerator instead of this CLI hack job
+      Rails::Generator::Scripts::Generate.new.run(generator_arguments)
+      redirect_to(new_conductor_scaffold_url)
+    end
+  
+  
+    private
+      def generator_arguments
+        returning [ "scaffold" ] do |args|
+          args << params[:scaffold][:name]
+          args << "--skip-timestamps" if params[:scaffold][:skip_timestamps] == "1"
+          args << "--skip-migration" if params[:scaffold][:skip_migration] == "1"
+          args << params[:scaffold][:fields].select { |f| f[:name].blank? }.collect { |f| "#{f[:name]}:#{f[:type]}" }
+        end.flatten
+      end
+  end
 end
