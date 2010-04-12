b4036307a83a2a1ecc68ede1c9283f6581056eb7
David Heinemeier Hansson <david@loudthinking.com>
2010-04-11 17:09:55 -0700

    Fixed routes viewer

--- app/controllers/conductor/routes_controller.rb
+++ app/controllers/conductor/routes_controller.rb
@@ -1,14 +1,13 @@
 module Conductor
   class RoutesController < ApplicationController
     def index
-      # TODO: Add this reflection on into the routing code and the replace this and the block in routing.rake with it
-      @routes = ActionController::Routing::Routes.routes.collect do |route|
-        name = ActionController::Routing::Routes.named_routes.routes.index(route).to_s
-        verb = route.conditions[:method].to_s.upcase
-        segs = route.segments.inject("") { |str,s| str << s.to_s }
-        segs.chop! if segs.length > 1
+      @routes = Rails.application.routes.routes.collect do |route|
+        name = Rails.application.routes.named_routes.routes.index(route).to_s
+        verb = route.verb.to_s
+        path = route.path
         reqs = route.requirements.empty? ? "" : route.requirements.inspect
-        {:name => name, :verb => verb, :segs => segs, :reqs => reqs}
+
+        { :name => name, :verb => verb, :path => path, :reqs => reqs }
       end
     end
   end
