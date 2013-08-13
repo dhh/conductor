a3649f54c75b6b9cdb9ca36e85444ddc9bd52a56
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-08-13 14:32:28 -0500

    Kernel#returning was deprecated in favor of Object#tap

--- app/controllers/conductor/scaffolds_controller.rb
+++ app/controllers/conductor/scaffolds_controller.rb
@@ -4,17 +4,17 @@ module Conductor
   class ScaffoldsController < ApplicationController
     def new
     end
-  
+
     def create
       # TODO: Change to use the proper ScaffoldGenerator instead of this CLI hack job
       Rails::Generators.invoke("scaffold", generator_arguments, :behavior => :invoke, :destination_root => Rails.root)
       redirect_to(new_conductor_scaffold_url)
     end
-  
-  
+
+
     private
       def generator_arguments
-        returning(Array.new) do |args|
+        Array.new.tap do |args|
           args << params[:scaffold][:name]
           args << "--skip-timestamps" if params[:scaffold][:skip_timestamps] == "1"
           args << "--skip-migration" if params[:scaffold][:skip_migration] == "1"
