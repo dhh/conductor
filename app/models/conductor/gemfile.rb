ff5f9b10246d555b8c9bff6e84be412466051a24
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-08-19 20:36:23 -0500

    Basic stub for Gemfile model

---
+++ app/models/conductor/gemfile.rb
@@ -0,0 +1,26 @@
+require 'singleton'
+
+module Conductor
+  class Gemfile
+    include Singleton
+
+    GEMFILE_PATH = File.join(Rails.root, 'Gemfile')
+
+    def content
+      @content ||= File.open(GEMFILE_PATH).read
+    end
+
+    def content=(content)
+      @content = content unless content.blank?
+    end
+
+    def save
+      output = content
+      !!File.open(GEMFILE_PATH, 'w') { |file| file.write(output) }
+    end
+
+    def to_s
+      content
+    end
+  end
+end
