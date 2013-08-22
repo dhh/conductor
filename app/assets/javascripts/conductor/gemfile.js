641f20996591f366978a1f0a29eb969d59bc8692
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-08-22 01:39:42 -0500

    Forget to remove some files...

--- app/assets/javascripts/conductor/gemfile.js
+++
@@ -1,7 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/github");
-    editor.getSession().setMode("ace/mode/ruby");
-
-
