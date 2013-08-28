2b428a71e59f5c4fb4f0a7eb5498344674190d70
Adriana Palacio <ppinedo.adriana@gmail.com>
2013-08-27 23:41:17 -0500

    Add routes for generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Conductor::Engine.routes.draw do
-  resources :scaffolds, :routes, :annotations, :statistics
+  resources :scaffolds, :routes, :annotations, :statistics, :resources, :models, :app_controllers, :mailers
   resource :gemfile, :database
 end
