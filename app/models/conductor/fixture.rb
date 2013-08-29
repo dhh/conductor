bb8312484153f1c838cad0fad34f7182c41a7928
lauragarcia <lauragarcia1993@hotmail.com>
2013-08-28 22:58:13 -0500

    Soft tabs in model fixture.rb

--- app/models/conductor/fixture.rb
+++ app/models/conductor/fixture.rb
@@ -1,10 +1,12 @@
 module Conductor
   class Fixture
+
     def self.files
       @files=[]
       #Dir[Rails.root.join("test/fixtures/**/*.yml")].each { |f|  @files << f }
       Dir["test/fixtures/**/*.yml"].each { |f|  @files << f }
       @files
-	end
+    end
+
   end
 end
