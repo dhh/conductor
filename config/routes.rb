cf588657903ca6b8d9cc8c224c1913abe67b086c
lauragarcia <lauragarcia1993@hotmail.com>
2013-08-30 12:12:27 -0500

    Create the Landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Conductor::Engine.routes.draw do
   resources :scaffolds, :routes, :annotations, :statistics, :resources, :models, :app_controllers, :mailers
   resource :gemfile, :database
-
+   get "welcome/index"
   resources :fixtures, except: [:edit, :update ,:show]
   get 'fixtures/*path', to: 'fixtures#edit', constraints: { path: /.*/ }
   patch 'fixtures/*path', to: 'fixtures#update', constraints: { path: /.*/ }
+  root "welcome#index"
 end
---
+++ test/helpers/conductor/welcome_helper_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module Conductor
+  class WelcomeHelperTest < ActionView::TestCase
+  end
+end
